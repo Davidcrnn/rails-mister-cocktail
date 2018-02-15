4ff3f62a0000a79757b7047a8dc1e4fcbc8d8544
David Crenin <david.crenin@gmail.com>
2018-02-15 16:41:27 +0100

    done

--- Gemfile
+++ Gemfile
@@ -4,6 +4,10 @@ git_source(:github) do |repo_name|
   repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
   "https://github.com/#{repo_name}.git"
 end
+gem 'autoprefixer-rails'
+gem 'bootstrap-sass' # Bootstrap v3
+gem 'font-awesome-sass'
+gem 'simple_form'
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+
+
+# Cocktail.create(name: "Mojito")
+# Cocktail.create(name: "Cuba Libre")
+# Cocktail.create(name: "Gin Tonic")
+# Cocktail.create(name: "Vodka Tonic")
+
+# Dose.create(description: "4cl", cocktail_id:, restaurant_id:)
+# Dose.create(description: "3cl", cocktail_id:, restaurant_id:)
+# Dose.create(description: "2cl", cocktail_id:, restaurant_id:)
+# Dose.create(description: "1cl", cocktail_id:, restaurant_id:)
+
+# # require 'json'
+# # require 'open-uri'
+
+# # url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+# # list_serialized = open(url).read
+# # user = JSON.parse(list_serialized)
+
+require ‘json’
+require ‘open-uri’
+
+Ingredient.destroy_all
+
+url = “http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list”
+list_serialized = open(url).read
+list = JSON.parse(list_serialized)
+
+list[“drinks”].each do |drink|
+ Ingredient.create!(name: drink[“strIngredient1”])
+end
